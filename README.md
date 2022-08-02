c5ec679baa18ee796c9e396f8bf484d0e4c86dfc
Dimitris Valilis <doomster@users.noreply.github.com>
2022-08-02 15:59:20 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# torn-custom-signatures
+Basic Live signature creation script for Torn.
+------------------------------------------------
+## Prerequesites: 
+* A file containing the apikeys in an array named as *FACTION_ID*-apikeys.php (api keys on the example are random , and will not work on the torn API. put your api keys (only requires public access)
+* Font files that you wanna use for your text. i am using UBUNTU and PNR
+* PNG background images 600x100 size named as *FACTION_ID*-1.png *FACTION_ID*-2.png etc. your member will decide which image to use by changing the link.  
+
+## what it does 
+The script chooses a random api key from the array on each image request, it then requests the user data from api. It checks if the user is a member of the faction, loads the image, imprints the text data to the image and serves the image as an output. If you use the link as an "insert image" link in torn, each time this image is to be displayed , the script will update the image with a new updated version.
+
+The more requests the more API keys are needed in order not to get the key "blocked" and get an empty response. for a full faction you will need around 10 keys. The more the better.
+
+If the player does not request a certain image to be displayed, it will show the 1st one (*-1.png)
+
+## Typical Folder / files structure example
+>torn-signature.php
+>
+>includes/11111-apikeys.php
+>
+>includes/11111-1.png
+>
+>includes/11111-2.png
+>
+>includes/11111-3.png
+>
+>includes/UBUNTU-MEDIUM.TTF
+>
+>includes/PRN.ttf
+
+The link to give you the signature with the default image is 
+
+**test.server.com/torn-signature.php?id=PLAYERS_TORN_ID&img=IMAGE_NUMBER
+
+
+#### If you do like this script, and find it usefull, i accept donations in the form of IG currency at hexxeh[2428617] . 
+
+#### If you have any questions, don't hesitate to contact me ingame by mail. 
+
+#### If you lack the experience to mod this script and/or to host it, i can mod/host it on my servers for your faction for a price. Lets talk!
---
+++ torn-signature.php
@@ -0,0 +1,55 @@
+<?php
+//set faction ID and faction name here.
+$factionid='ADD_FACTION_ID_HERE';
+$factioname='ADD_FACTION_NAME_HERE';
+//load random api key from list
+require 'includes/'.$factionid.'-apikeys.php';
+$randmax = count($apikeys);
+$rand = rand(0,$randmax);
+//request data from server
+$statusrequest = file_get_contents('http://api.torn.com/user/'.$_GET['id'].'?selections=&key='.$apikeys[$rand]);
+//check if there is an image request, or default to 1st one
+if(isset($_GET['img'])) { $selectedimage = $_GET['img']; } else { $selectedimage = '1' ; }
+$status = json_decode($statusrequest);
+//setup variables
+$faction_check = $status->faction->faction_id;
+$faction_rank = $status->faction->position;
+$faction_name = $status->faction->faction_name;
+//check if he is a member of the faction. Allways change the name here according to the faction.
+if ($faction_check != $factionid) { die('Not a member of '.$factioname); }
+// REQUESTS COUNTER (uncomment next four lines to get a counter for the requests, for usage purposes)
+//$file = 'requestslog.txt';
+//$fdata = file_get_contents ( $file );
+//$fdata = intval($fdata) + 1;
+//file_put_contents($file, $fdata);
+
+// Firstly open the image
+$selectedimage = 'includes/'.$factionid.'-'.$selectedimage.'.png';
+$img = imagecreatefrompng($selectedimage);
+// Set text properties
+$fontsize = 10;
+$rankfontsize = 30;
+$angle = 0;
+$white = imagecolorallocate($img, 255, 255, 255);
+$black = imagecolorallocate($img, 0, 0, 0);
+$font = "includes/UBUNTU-MEDIUM.TTF";
+$rankfont = "includes/PNR.ttf";
+//Add the text
+$txt = $status->name.'['.$status->player_id.']  |  Level:'.$status->level.'  |  Age:'.$status->age.'  |  '.$stat
+$txt2 = $status->faction->position.' of '.$factionname ;
+//Calculate text size to center it
+$image_width = imagesx($img);
+$text_box = imagettfbbox($fontsize,$angle,$font,$txt);
+$text2_box = imagettfbbox($rankfontsize,$angle,$rankfont,$txt2);
+$text_width = $text_box[2]-$text_box[0];
+$text2_width = $text2_box[2] - $text2_box[0];
+$x = ($image_width/2) - ($text_width/2);
+$x2 = ($image_width/2) - ($text2_width/2);
+//Write text to image
+imagettftext($img, $fontsize, $angle, $x, 15, $white, $font, $txt);
+imagettftext($img, $rankfontsize, $angle, $x2, 75, $white, $rankfont, $txt2);
+//Image Output
+header('Content-type: image/png');
+imagepng($img);
+imagedestroy($img);
+?>
